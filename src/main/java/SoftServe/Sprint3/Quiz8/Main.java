9a0cbb6568620ea8a13b5fe7b03383ca90d75e49
ElnurKasimov <ekasimov2012@gmail.com>
2023-07-09 18:20:49 +0300

    added Sprint4 Theory - bubble sorting

--- src/main/java/SoftServe/Sprint3/Quiz8/Main.java
+++ src/main/java/SoftServe/Sprint3/Quiz8/Main.java
@@ -2,13 +2,13 @@ package SoftServe.Sprint3.Quiz8;
 
 public class Main {
     public static void main(String[] args) {
-        Shape shape = new Shape() {
-            String color = "Green";
-            public void draw() {
-                System.out.println("shape is " + color);
-            }
-        };
-            shape.draw();
-    }
+//        Shape shape = new Shape() {
+//            String color = "Green";
+//            public void draw() {
+//                System.out.println("shape is " + color);
+//            }
+//        };
+//            shape.draw();
+   }
 }
 
