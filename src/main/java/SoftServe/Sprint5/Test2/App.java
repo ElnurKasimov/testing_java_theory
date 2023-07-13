84413dfb15a5f7ae02974e78261a2523960d44fa
Elnur <ekasimov2012@gmail.com>
2023-07-13 17:22:28 +0300

    some refactoring has been carried out

--- src/main/java/SoftServe/Sprint5/Test2/App.java
+++ src/main/java/SoftServe/Sprint5/Test2/App.java
@@ -2,6 +2,7 @@ package SoftServe.Sprint5.Test2;
 
 import java.util.Arrays;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class App {
     public static Consumer<double[]> cons = array -> {
@@ -17,7 +18,18 @@ public class App {
     }
 
     public static void main(String[] args) {
-        double[] actual = App.getChanged(new double[] { 1, 2, 3, -1, -5, 0 }, App.cons);
+        double[] actual = new double[] { 1., 2., 3., -1., -5., 0. };
+        double[] result = App.getChanged(actual, App.cons);
         System.out.println(Arrays.toString(actual));
+        System.out.println(Arrays.toString(result));
+        Supplier<Double> sup = () -> {
+            double average = 0.;
+            double sum = 0;
+            for(double number : actual) {
+                sum+=number;
+            }
+            return sum/actual.length;
+        };
+        System.out.println(" result = " + sup.get());
     }
 }
