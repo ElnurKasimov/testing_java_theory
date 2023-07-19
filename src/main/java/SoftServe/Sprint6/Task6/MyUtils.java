282152a86ec60ea7c46115e0722008f7a026584e
Elnur <ekasimov2012@gmail.com>
2023-07-19 12:28:56 +0300

    amended Task6

--- src/main/java/SoftServe/Sprint6/Task6/MyUtils.java
+++ src/main/java/SoftServe/Sprint6/Task6/MyUtils.java
@@ -10,70 +10,77 @@ public class MyUtils {
 
     public static Map<String, Stream<String>> phoneNumbers(List<Stream<String>> list) {
         Map<String, Stream<String>> result = new HashMap<>();
-        List<String> err = list.stream()
+        List<Stream<String>> list1 = list.stream().toList();
+        List<Stream<String>> list2 = list.stream().toList();
+        List<Stream<String>> list3 = list.stream().toList();
+        Map<String, Stream<String>> mob = list1.stream()
                 .filter(stream -> stream != null)
-                .filter(stream -> stream.count() > 1)
-                .flatMap(stream -> stream)
-                .map(str -> str.replaceAll("\\(", ""))
-                .map(str -> str.replaceAll("\\)", ""))
-                .map(str -> str.replaceAll("-", ""))
-                .map(str -> str.replaceAll("\\s", ""))
-                .filter(s -> !s.trim().isEmpty())
-                .distinct()
-                .filter(s -> s.length() < 7)
-                .sorted().toList();
-        Map<String, Stream<String>> errMap = new HashMap<>();
-        errMap.put("err",err.stream());
-
-        List<String> loc = list.stream()
-                .filter(stream -> stream != null)
-                .filter(stream -> stream.count() > 1)
-                .flatMap(stream -> stream)
-                .map(str -> str.replaceAll("\\(", ""))
-                .map(str -> str.replaceAll("\\)", ""))
-                .map(str -> str.replaceAll("-", ""))
-                .map(str -> str.replaceAll("\\s", ""))
-                .filter(s -> !s.trim().isEmpty())
-                .distinct()
-                .filter(s -> s.length() == 7)
-                .sorted().toList();
-        Map<String, Stream<String>> locMap = new HashMap<>();
-        locMap.put("loc",loc.stream());
-
-        Map<String, Stream<String>> mobMap = list.stream()
-                .filter(stream -> stream != null)
-                .filter(stream -> stream.count() > 1)
                 .flatMap(stream->stream)
                 .map(str -> str.replaceAll("\\(", ""))
                 .map(str -> str.replaceAll("\\)", ""))
                 .map(str -> str.replaceAll("-", ""))
                 .map(str -> str.replaceAll("\\s", ""))
+                .filter(s -> s != null)
                 .filter(s -> !s.trim().isEmpty())
-                .distinct()
-                .filter(s -> s.length() > 7)
                 .sorted()
-                .collect(Collectors.groupingBy(
-                        str -> str.substring(0,3),
-                        Collectors.mapping(str -> str.substring(3), Collectors.toList() )
-                ))
-                .entrySet()
-                .stream()
+                .distinct()
+                .filter(s -> s.length() == 10)
                 .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        entry -> entry.getValue().stream()
-                ));
-        result.putAll(errMap);
-        result.putAll(locMap);
-        result.putAll(mobMap);
+                        (k) -> k.substring(0,3),
+                        (v) -> Stream.of(v.substring(3)), Stream::concat));
+
+        Map<String, Stream<String>> loc = list2.stream()
+                .filter(stream -> stream != null)
+                .flatMap(stream->stream)
+                .map(str -> str.replaceAll("\\(", ""))
+                .map(str -> str.replaceAll("\\)", ""))
+                .map(str -> str.replaceAll("-", ""))
+                .map(str -> str.replaceAll("\\s", ""))
+                .filter(s -> s != null)
+                .filter(s -> !s.trim().isEmpty())
+                .sorted()
+                .distinct()
+                .filter(s -> s.length() == 7)
+                .collect(Collectors.toMap(
+                        (k) -> "loc",
+                        (v) -> Stream.of(v), Stream::concat));
+
+        Map<String, Stream<String>> err = list3.stream()
+                .filter(stream -> stream != null)
+                .flatMap(stream->stream)
+                .map(str -> str.replaceAll("\\(", ""))
+                .map(str -> str.replaceAll("\\)", ""))
+                .map(str -> str.replaceAll("-", ""))
+                .map(str -> str.replaceAll("\\s", ""))
+                .filter(s -> s != null)
+                .filter(s -> !s.trim().isEmpty())
+                .sorted()
+                .distinct()
+                .filter(s -> s.length()!=7 && s.length()!=10)
+                .collect(Collectors.toMap(
+                        (k) -> "err",
+                        (v) -> Stream.of(v), Stream::concat));
+        result.putAll(mob);
+        result.putAll(loc);
+        result.putAll(err);
+
         return result;
     }
 
     public static void main(String[] args) {
         List<Stream<String>> given = List.of(
                 Stream.of("093 987 65 43", "(050)1234567", "12-345"),
+                Stream.of("0999999999"),
                 Stream.of("067-21-436-57", "050-2345678", "0939182736", "224-19-28"),
                 Stream.of("(093)-11-22-334", "044 435-62-18", "721-73-45"));
-        System.out.println(MyUtils.phoneNumbers(given));
+        for(Map.Entry entry : MyUtils.phoneNumbers(given).entrySet()) {
+            System.out.print(entry.getKey() + "  ");
+            Stream<String> str = (Stream) entry.getValue();
+            str.forEach(s -> System.out.print(" " + s + ","));
+            System.out.println();
+        }
+//        MyUtils.phoneNumbers(given).forEach((key, value) -> {
+//       System.out.println(key + " -> " + value.collect(Collectors.joining(", ")));
+//        });
     }
-
 }
