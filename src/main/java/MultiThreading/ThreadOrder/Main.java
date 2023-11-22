9f63ee25e17d1b70477cbd409427c5355e1898f3
ElnurKasimov <ekasimov2012@gmail.com>
2023-11-22 22:40:16 +0200

    testing differ behaviour of threads in ThreadOrder

--- src/main/java/MultiThreading/ThreadOrder/Main.java
+++ src/main/java/MultiThreading/ThreadOrder/Main.java
@@ -4,10 +4,8 @@ public class Main {
 
     public static void main(String []args) {
         System.out.println("Hello Main");
-
         HelloThread2 t2 = new HelloThread2();
         t2.start();
-
         HelloThread1 t1 = new HelloThread1();
         t1.start();
     }
