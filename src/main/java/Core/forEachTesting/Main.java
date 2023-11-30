96113d21a16ab63cb2e2dfd101ed1ecc177fe144
ElnurKasimov <ekasimov2012@gmail.com>
2023-11-30 19:50:49 +0200

    testing behaviour of the loop forEach

---
+++ src/main/java/Core/forEachTesting/Main.java
@@ -0,0 +1,25 @@
+package Core.forEachTesting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person("first", 20));
+        personList.add(new Person("second", 25));
+        personList.add(new Person("third", 30));
+        System.out.println(personList.toString());
+        for (Person person : personList) {
+            if(person.getAge() == 25) {
+                person.setName("SECOND");
+            }
+        }
+        System.out.println(personList.toString());
+
+    }
+
+}
