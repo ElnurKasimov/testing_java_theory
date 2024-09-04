dc549cecae21b38d3d340c74bda279447a119859
Elnur Kasimov <ekasimov2012@gmail.com>
2024-09-04 14:46:34 +0300

    added StringModifier, Fibonacci into HyperSkill.Debugger

---
+++ src/main/java/HyperSkill/Debugger/Fibonacci.java
@@ -0,0 +1,16 @@
+package HyperSkill.Debugger;
+
+public class Fibonacci {
+        public static void main(String[] args) {
+            int fibonacciPrevious = 1;
+            int fibonacciCurrent = 1;
+            int fibonacciSum = fibonacciPrevious + fibonacciCurrent;
+
+            while (true) {
+                int tmp = fibonacciPrevious + fibonacciCurrent;
+                fibonacciPrevious = fibonacciCurrent;
+                fibonacciCurrent = tmp;
+                fibonacciSum += fibonacciCurrent;
+            }
+        }
+    }
